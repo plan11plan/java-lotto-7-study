13791a471234764f9c5949818fc01ae5719f9b6f
plan11plan <tjtfy7028@gmail.com>
2024-11-17 19:48:25 +0900

    feat: InputView에서 당첨 번호 입력 받기 기능 구현

--- src/main/java/lotto/ConsoleInputView.java
+++ src/main/java/lotto/ConsoleInputView.java
@@ -5,6 +5,8 @@ import static lotto.ErrorMessage.*;
 import static lotto.LottoRule.*;
 import static 템플릿_가져온것들.util.RangeValidator.*;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import 템플릿_가져온것들.if_throw.common.validation.Validator;
 import 템플릿_가져온것들.util.RangeValidator;
@@ -26,6 +28,7 @@ import 템플릿_가져온것들.util.RangeValidator;
  *
  */
 public class ConsoleInputView {
+    private final String DELIMITER_COMMA =",";
     /**
      * -[x] 로또 가격은 1,000원 ~ 10,000원 까지 입니다.
      * -[x] 로또 가격은 1,000원 단위 입니다.
@@ -43,9 +46,27 @@ public class ConsoleInputView {
         return fee;
     }
 
+    /**
+     * 당첨 번호
+     *  - 로또 번호는 6개여야 합니다.
+     *  - 로또 번호의 숫자 범위는 1~45까지 입니다.
+     *  - 로또 번호는 서로 중복될 수 없습니다.
+     */
     public List<Integer> requestPrizeLottos() {
+        List<Integer> prizeLottos = Arrays.stream(consoleRead().trim().split(DELIMITER_COMMA))
+                .map(Integer::parseInt)
+                .toList();
+        Validator
+                .check(prizeLottos.size() != LOTTO_SIZE.getCriteria())
+                .withError(new IllegalArgumentException(INVALID_LOTTO_COUNT.getMessage()))
 
-        return null;
+                .check(isNumberInRange(prizeLottos, LOTTO_MIN_VALUE.getCriteria(), LOTTO_MAX_VALUE.getCriteria()))
+                .withError(new IllegalArgumentException(INVALID_LOTTO_RANGE.getMessage()))
+
+                .check(new HashSet<>(prizeLottos).size() != LOTTO_SIZE.getCriteria())
+                .withError(new IllegalArgumentException(INVALID_LOTTO_NUMBER_DUPLICATE.getMessage()))
+                .validate();
+       return prizeLottos;
     }
 
     public int requestBonusLotto() {
