8413df194ebeb7c15a7c75d7dd38b166a4a67d69
plan11plan <tjtfy7028@gmail.com>
2024-11-17 19:47:52 +0900

    feat: RangeValidator 숫자 리스트 범위 판별 기능 구현

--- src/main/java/템플릿_가져온것들/util/RangeValidator.java
+++ src/main/java/템플릿_가져온것들/util/RangeValidator.java
@@ -1,5 +1,7 @@
 package 템플릿_가져온것들.util;
 
+import java.util.List;
+
 public class RangeValidator {
     private RangeValidator() {
     }
@@ -7,6 +9,14 @@ public class RangeValidator {
     public static boolean isNumberInRange(int number, int min, int max) {
         return number >= min && number <= max;
     }
+    // 새로 다시 공부 하게 된 부분. allMatch ( boolean 타입을 반환할때는 allMatch)
+    public static boolean isNumberInRange(List<Integer> numbers, int min, int max) {
+        return numbers.stream()
+                .allMatch(num -> isNumberInRange(num, min, max));
+    }
+//    public static boolean isNumberInRange(List<Integer> numbers, int min, int max){
+//        return numbers.stream().forEach(num-> isNumberInRange(num,min,max));
+//    }
 
     public static boolean isPositive(int number) {
         return number > 0;
