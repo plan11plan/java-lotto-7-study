8f01eb68feacb8a5d4d49970f72a471008701563
plan11plan <tjtfy7028@gmail.com>
2024-11-17 23:07:55 +0900

    refactor: 추가 검증 기능 구현
    - 로또 중복 불가

--- src/main/java/lotto/domain/lottoMachine/Lotto.java
+++ src/main/java/lotto/domain/lottoMachine/Lotto.java
@@ -1,8 +1,14 @@
 package lotto.domain.lottoMachine;
 
+import static lotto.common.ErrorMessage.INVALID_LOTTO_NUMBER_DUPLICATE;
+import static lotto.common.LottoRule.LOTTO_SIZE;
+
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import lotto.domain.prizeMachine.PickedWinnerLottos;
 import lotto.domain.prizeMachine.PrizeLotto;
+import 템플릿_가져온것들.if_throw.common.validation.Validator;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,9 +19,11 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+        Validator.check(new HashSet<>(numbers).size() != LOTTO_SIZE.getCriteria())
+                .withError(new IllegalArgumentException(INVALID_LOTTO_NUMBER_DUPLICATE.getMessage()))
+                .check(numbers.size() != LOTTO_SIZE.getCriteria())
+                .withError(new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다."))
+                .validate();
     }
 
     // TODO: 추가 기능 구현
