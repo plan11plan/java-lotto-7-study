4041b9ef4dab63df2ba0a22d803a336f2ec29dce
plan11plan <tjtfy7028@gmail.com>
2024-11-17 21:23:24 +0900

    refactor: 불변 타입 추가

--- src/main/java/lotto/domain/lottoMachine/PickedLottos.java
+++ src/main/java/lotto/domain/lottoMachine/PickedLottos.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class PickedLottos {
-    List<Lotto> lottos;
+    private final List<Lotto> lottos;
     public PickedLottos(final List<List<Integer>> generate) {
         this.lottos = generate.stream()
                 .map(Lotto::new)
