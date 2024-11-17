16ce79b634236fb506ec957bbb407bdcbbf546f0
plan11plan <tjtfy7028@gmail.com>
2024-11-17 19:16:25 +0900

    feat: 로또 규칙 Enum으로 한 곳에 관리

---
+++ src/main/java/lotto/LottoRule.java
@@ -0,0 +1,22 @@
+package lotto;
+
+public enum LottoRule {
+
+    BUY_MIN_PRICE(1000),
+    BUY_MAX_PRICE(10_000),
+    LOTTO_PRICE(1000),
+    LOTTO_MIN_VALUE(1),
+    LOTTO_MAX_VALUE(45);
+
+
+    private final int criteria;
+
+    LottoRule(int criteria) {
+        this.criteria = criteria;
+    }
+
+    public int getCriteria() {
+        return criteria;
+    }
+
+}
