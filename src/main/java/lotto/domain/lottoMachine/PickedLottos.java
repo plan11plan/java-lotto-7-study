0e9fa810506ddff1dc049ab5730512c7a1f7d3d5
plan11plan <tjtfy7028@gmail.com>
2024-11-17 20:52:48 +0900

    feat: PickedLottos에서 로또 리스트 관리 기능 구현

--- src/main/java/lotto/domain/lottoMachine/PickedLottos.java
+++ src/main/java/lotto/domain/lottoMachine/PickedLottos.java
@@ -1,4 +1,13 @@
 package lotto.domain.lottoMachine;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PickedLottos {
+    List<Lotto> lottos;
+    public PickedLottos(final List<List<Integer>> generate) {
+        this.lottos = generate.stream()
+                .map(Lotto::new)
+                .toList();
+    }
 }
